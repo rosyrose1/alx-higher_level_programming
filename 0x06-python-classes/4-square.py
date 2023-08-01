afc0e6ea1542302df39e12fb16e65208a7a49154
rosyrose1 <128856014+rosyrose1@users.noreply.github.com>
2023-08-01 13:56:34 +0000

    Create 4-square.py
     a class Square that defines a square by: (based on 3-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Library  definition of class 'Square'
+"""
+class Square():
+    """ Specifying a 'Square'
+    """
+    def __init__(self, size=0):
+        """ Initiation of a 'Square'
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """ Get the size of a square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """ position the size of a square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Compute the area of a 'Square'
+        """
+        return self.__size ** 2
