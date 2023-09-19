d20f040d48aae6a8e4a2f4f043a251916d59d5c5
rosyrose1 <rosinaantwi96@gmail.com>
2023-09-19 06:52:53 -0700

    -- create first_table in current database if it do not already exist

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,2 @@
 -- create first_table in current database if it do not already exist
-CREATE TABLE IF NOT EXISTS first_name(
-id INT,
-name VARCHAR(250));
+CREATE TABLE IF NOT EXISTS first_name (id INT, name VARCHAR(256));
