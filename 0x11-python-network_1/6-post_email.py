e115e7fc18cd00966f688ba973691190811a6a1a
rosyrose1 <rosinaantwi96@gmail.com>
2023-11-03 07:03:39 -0700

    python network 1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+"""
+import sys
+import requests
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    res = requests.post(url, data=value)
+    print(res.text)
